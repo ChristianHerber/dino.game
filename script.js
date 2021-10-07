16d1549a26ef3f9eb7d7db9a2f821a83f5962aef
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-10-07 09:50:35 -0400

    dando movimento e criando cactos aleatorios

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function handleKeyUp(event){
         }
         
     }
-}
+}//handleKeyUp
 
 function jump(){
     let position = 0
@@ -34,15 +34,34 @@ function jump(){
             dino.style.bottom = `${position}px`
         }
     }, 20)
-}
+}//jump
 
 function createCactus(){
     const cactus = document.createElement('div')
     let cactusPosition = 1000
+    let randomTime = Math.random() * 6000
+
     cactus.classList.add('cactus')
     cactus.style.left = 1000 + 'px'
     background.appendChild(cactus)
-}
+
+    let leftInterval = setInterval( () => {
+        cactusPosition -= 1 
+        cactus.style.left = `${cactusPosition}px`
+
+        if(cactusPosition < -60){
+            clearInterval(leftInterval)
+            background.removeChild(cactus)
+        } else {
+            cactusPosition -= 10
+            cactus.style.left = `${cactusPosition}px`
+        }
+
+    }, 20)
+
+    setTimeout(createCactus, randomTime)
+
+} //createCactus
 
 createCactus()
 document.addEventListener('keyup', handleKeyUp)
