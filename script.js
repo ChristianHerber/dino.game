98f0e1cfbb2ab6364eb4bf6670d860b26cda0acd
Christian Herber <christian@tangaradaserra.mt.gov.br>
2021-10-07 10:12:27 -0400

    criando o contato do cacto no dino

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector(".dino")
 const background = document.querySelector(".background")
 let isJumping = false
+let position = 0
 
 function handleKeyUp(event){
     if(event.keyCode === 32){
@@ -12,8 +13,6 @@ function handleKeyUp(event){
 }//handleKeyUp
 
 function jump(){
-    let position = 0
-
     isJumping = true
 
     let upInterval = setInterval( () => {
@@ -52,6 +51,10 @@ function createCactus(){
         if(cactusPosition < -60){
             clearInterval(leftInterval)
             background.removeChild(cactus)
+        } else if(cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //Game Over
+            clearInterval(leftInterval)
+            document.body.innerHTML = '<h1 class="gameover">Fim de Jogo</h1>'
         } else {
             cactusPosition -= 10
             cactus.style.left = `${cactusPosition}px`
